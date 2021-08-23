a82d07a14296694432f4aef5a5fab2d0277ac57d
LOctavio <luis_ram1997@hotmail.com>
2021-08-23 13:21:28 -0500

    Change checked state when click checkbox

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -20,12 +20,24 @@ class TodoContainer extends React.Component {
         completed: false
       }
     ]
-   };
+  };
+
+  handleChange = (id) => {
+    this.setState({
+      todos: this.state.todos.map(todo => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed;
+        }
+        return todo;
+      })
+    });
+  };
+
    render() {
     return (
       <div>
-         <Header />
-        <TodosList todos={this.state.todos} />
+        <Header />
+        <TodosList todos={this.state.todos} handleChangeProps={this.handleChange} />
       </div>
     );
   }
