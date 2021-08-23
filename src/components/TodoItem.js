348fbd66dece5dd339e6a326d37a0f7e36f9e10a
LOctavio <luis_ram1997@hotmail.com>
2021-08-23 17:02:06 -0500

    Add styles when any of the items is completed

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -2,6 +2,13 @@ import React from "react"
 import styles from "./TodoItem.module.css"
 
 function TodoItem(props) {
+  const completedStyle = {
+    fontStyle: "italic",
+    color: "#595959",
+    opacity: 0.4,
+    textDecoration: "line-through",
+  }
+
   return (
     <li className={styles.item}>
       <input
@@ -11,7 +18,7 @@ function TodoItem(props) {
         onChange={() => props.handleChangeProps(props.todo.id)}
       />
       <button onClick={() => props.deleteTodoProps(props.todo.id)}>Delete</button>
-      {props.todo.title}
+      <span style={props.todo.completed ? completedStyle : null}>{props.todo.title}</span>
     </li>
   )
 }
