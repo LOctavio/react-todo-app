fecb67d5c887ca02d62991f99bd2b7c52586da00
LOctavio <luis_ram1997@hotmail.com>
2021-08-25 16:16:50 -0500

    Add forder for class and function components

---
+++ src/functionBased/components/InputTodo.js
@@ -0,0 +1,54 @@
+import { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class InputTodo extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: '',
+    };
+  }
+
+  onChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  handleSubmit = (e) => {
+    const { title } = this.state;
+    const { props } = this;
+    e.preventDefault();
+    if (title.trim()) {
+      props.addTodoProps(title);
+      this.setState({
+        title: '',
+      });
+    } else {
+      alert('Please write item');
+    }
+  };
+
+  render() {
+    const { title } = this.state;
+    return (
+      <form onSubmit={this.handleSubmit} className="form-container">
+        <input
+          type="text"
+          className="input-text"
+          placeholder="Add Todo..."
+          value={title}
+          name="title"
+          onChange={this.onChange}
+        />
+        <button type="submit" className="input-submit">Submit</button>
+      </form>
+    );
+  }
+}
+
+InputTodo.propTypes = {
+  addTodoProps: PropTypes.func.isRequired,
+};
+
+export default InputTodo;
---
+++ src/functionBased/components/TodosList.js
@@ -0,0 +1,30 @@
+import PropTypes from 'prop-types';
+import TodoItem from './TodoItem';
+
+function TodosList(props) {
+  const {
+    handleChangeProps, deleteTodoProps, setUpdate, todos,
+  } = props;
+  return (
+    <ul>
+      {todos.map((todo) => (
+        <TodoItem
+          key={todo.id}
+          todo={todo}
+          handleChangeProps={handleChangeProps}
+          deleteTodoProps={deleteTodoProps}
+          setUpdate={setUpdate}
+        />
+      ))}
+    </ul>
+  );
+}
+
+TodosList.propTypes = {
+  todos: PropTypes.arrayOf(PropTypes.shape()).isRequired,
+  handleChangeProps: PropTypes.func.isRequired,
+  deleteTodoProps: PropTypes.func.isRequired,
+  setUpdate: PropTypes.func.isRequired,
+};
+
+export default TodosList;
